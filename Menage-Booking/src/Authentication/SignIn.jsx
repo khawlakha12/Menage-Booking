c93b964fc34cfa3f89025221fd3affbf1ec04315
khawlakha12 <kharbouchikhawla603@gmail.com>
2025-11-22 22:36:04 +0100

    instalation react and tailwind

---
+++ Menage-Booking/src/Authentication/SignIn.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function SignIn() {
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  return (
+    <div className="w-[430px] bg-white p-8 rounded-2xl shadow-lg">
+
+    </div>
+  );
+}
+
+export default SignIn;
