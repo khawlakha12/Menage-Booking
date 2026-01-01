fd1bbc180c9b3f15f68e50a20996873f6f321da7
khawlakha12 <kharbouchikhawla603@gmail.com>
2026-01-01 17:26:42 +0100

    changer logo

--- Menage-Booking/src/components/Home/hero.tsx
+++ Menage-Booking/src/components/Home/hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
 <div className="flex items-center space-x-2">
-  <div className="w-20 h-20 rounded-lg overflow-hidden flex items-center justify-center">
+  <div className="w-auto h-20 rounded-lg overflow-hidden flex items-center justify-center">
      <img src="image/logo.png" alt="Logo" className="w-full h-full object-cover" />
   </div>
 </div>
