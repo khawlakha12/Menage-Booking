6519aa66dcb0f91470ff6db817b8c51bb80c8384
khawlakha12 <kharbouchikhawla603@gmail.com>
2026-01-01 18:04:21 +0100

    changer logo et icon

--- Menage-Booking/src/pages/About.tsx
+++ Menage-Booking/src/pages/About.tsx
@@ -14,12 +14,12 @@ export default function About() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
-            <div className="flex items-center space-x-2">
-              <div className="w-8 h-8 bg-gradient-to-br from-blue-500 to-blue-600 rounded-lg flex items-center justify-center">
-                <span className="text-white font-bold text-xl">M</span>
-              </div>
-              <span className={`text-xl font-semibold ${isDark ? 'text-white' : 'text-gray-900'}`}>Booking</span>
-            </div>
+           <div className="flex items-center space-x-2">
+  <div className="w-auto h-16 rounded-lg overflow-hidden flex items-center justify-center">
+     <img src="image/logo.png" alt="Logo" className="w-full h-full object-cover" />
+  </div>
+</div>
+
 
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center space-x-8">
