1c67ef0eb7dc43525c3e579523c4f3c53ced1ce6
khawlakha12 <kharbouchikhawla603@gmail.com>
2026-01-01 17:05:59 +0100

    changer size logo

--- Menage-Booking/src/components/Home/hero.tsx
+++ Menage-Booking/src/components/Home/hero.tsx
@@ -15,11 +15,12 @@ export default function Hero() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
-<div className="flex items-center space-x-2">
+
+
   <div className="w-20 h-20 rounded-lg overflow-hidden flex items-center justify-center">
-     <img src="../../../public/image/Logo_menage-removebg.png" alt="Logo" className="w-full h-full object-cover" />
+     <img src="image/Logo_menage-removebg.png" alt="Logo" className="w-full h-full object-cover" />
   </div>
-</div>
+
 
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center space-x-8">
